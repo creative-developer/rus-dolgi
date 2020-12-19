890e391f623c81d39087c1f4dbdfcf0fd0641fce
Serdar Gurbanmyradov <sgurbanmyradov@i-neti.ru>
2020-12-19 16:43:13 +0300

    main form

--- app/js/modules/select.js
+++ app/js/modules/select.js
@@ -1,31 +1,31 @@
-import { validator } from "./validation";
+import { validator } from './validation'
 
 export const addSelectArrow = () => {
   const arrow =
-    '<svg class="icon icon--arrow-down"><use xlink:href="img/svg-sprite.svg#arrow-down"></use></svg>';
-  $(".jq-selectbox__select .jq-selectbox__trigger").html(arrow);
-};
+    '<svg class="icon icon--triangle-down"><use xlink:href="img/svg-sprite.svg#triangle-down"></use></svg>'
+  $('.jq-selectbox__select .jq-selectbox__trigger').html(arrow)
+}
 
 export const customSelectInit = () => {
   const selectInit = () => {
-    $("select").styler({
+    $('select').styler({
       selectSearch: true,
       selectSearchLimit: 6,
-      selectPlaceholder: "Выберите регион",
+      selectPlaceholder: 'Выберите регион',
       selectSmartPositioning: false,
       onSelectOpened: function () {
-        $(this).find(".jq-selectbox__search input").focus();
+        // $(this).find(".jq-selectbox__search input").focus();
       },
       onSelectClosed: function () {
-        validator.element("select");
+        // validator.element('select')
       },
       onFormStyled: function () {
-        addSelectArrow();
+        addSelectArrow()
       },
-    });
-  };
-  $(".select-wrap__label").click(() => {
-    selectInit();
-  });
-  selectInit();
-};
+    })
+  }
+  // $('.select__label').click(() => {
+  //   selectInit()
+  // })
+  selectInit()
+}
