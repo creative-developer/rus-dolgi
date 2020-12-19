ced01b37eccc8ecd90aff57689c44ea47491af49
Serdar Gurbanmyradov <sgurbanmyradov@i-neti.ru>
2020-12-19 08:56:38 +0300

    start template

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack')
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      },
+    },
+  ]
+
+  return loaders
+}
+
+const config = {
+  entry: {
+    main: './app/js/main.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+}
+
+module.exports = config
